2997a3cb6ad639570b22bbc8f85e8fa898a817c2
Dilbrino <dilbrinh@hiof.no>
2023-05-27 01:14:15 +0200

    create a batched renderer

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -50,6 +50,17 @@ public class Window {
                     break;
             }
         }
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+
+    public static Scene getScene() {
+        return get().currentScene;
+    }
 
     public void run(){
         System.out.println("Hello opengl" + Version.getVersion() + "!");
